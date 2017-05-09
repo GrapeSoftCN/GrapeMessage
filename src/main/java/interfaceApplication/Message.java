f1271adc6099f88ede89ed4cfc7b67653d52fa5c
GrapeSoftCN <jk282@163.com>
2017-05-09 21:33:19 +0800

    更换框架包

--- src/main/java/interfaceApplication/Message.java
+++ src/main/java/interfaceApplication/Message.java
@@ -13,10 +13,10 @@ import rpc.execRequest;
 public class Message {
 	private MessageModel model = new MessageModel();
 	private HashMap<String, Object> map = new HashMap<>();
-	private String userid;
+//	private String userid;
 
 	public Message() {
-		userid = execRequest.getChannelValue("Userid").toString();
+//		userid = execRequest.getChannelValue("Userid").toString();
 
 		map.put("messageDate", TimeHelper.nowMillis() + "");
 		map.put("floor", model.getFloor());
@@ -44,9 +44,9 @@ public class Message {
 		JSONObject object = model.AddMap(map, JSONHelper.string2json(msgInfo));
 		if (object.containsKey("fatherid")) {
 			if (!object.get("fatherid").toString().equals("0")) {
-				if (!model.getUPLV(object.get("fatherid").toString(), userid)) {
-					return model.resultMessage(3, "没有回复权限");
-				}
+//				if (!model.getUPLV(object.get("fatherid").toString(), userid)) {
+//					return model.resultMessage(3, "没有回复权限");
+//				}
 				object.remove("floor");
 				String replynum = String.valueOf(
 						model.countReply(object.get("fatherid").toString())+1);
@@ -58,18 +58,18 @@ public class Message {
 			}
 		}
 		if (code == 0) {
-			String tip = execRequest
-					._run("GrapeAuth/Auth/InsertPLV/s:" + userid, null)
-					.toString();
-			if (!"0".equals(tip)) {
+//			String tip = execRequest
+//					._run("GrapeAuth/Auth/InsertPLV/s:" + userid, null)
+//					.toString();
+//			if (!"0".equals(tip)) {
 				//没有新增权限，则回复数-1
 				String replynum = String.valueOf(
 						model.countReply(object.get("fatherid").toString())-1);
 				temp.put("replynum", Integer.parseInt(replynum));
 				code = model.updateMessage(object.get("fatherid").toString(),
 						temp);
-				return model.resultMessage(3, "");
-			}
+//				return model.resultMessage(3, "");
+//			}
 			result = JSONHelper.string2json(model.addMessage(object));
 		}
 		return model.resultMessage(result);
@@ -77,9 +77,9 @@ public class Message {
 
 	// 修改留言
 	public String UpdateMessage(String mid, String msgInfo) {
-		if (!model.getUPLV(mid, userid)) {
-			return model.resultMessage(3, "没有修改权限");
-		}
+//		if (!model.getUPLV(mid, userid)) {
+//			return model.resultMessage(3, "没有修改权限");
+//		}
 		return model.resultMessage(
 				model.updateMessage(mid, JSONHelper.string2json(msgInfo)),
 				"留言修改成功");
@@ -87,14 +87,14 @@ public class Message {
 
 	// 删除留言
 	public String DeleteMessage(String mid) {
-		String dPLV = model.find(mid).get("dplv").toString();
-		String tip = execRequest
-				._run("GrapeAuth/Auth/UpdatePLV/s:" + dPLV + "/s:" + userid,
-						null)
-				.toString();
-		if (!"0".equals(tip)) {
-			return model.resultMessage(4, "没有删除权限");
-		}
+//		String dPLV = model.find(mid).get("dplv").toString();
+//		String tip = execRequest
+//				._run("GrapeAuth/Auth/UpdatePLV/s:" + dPLV + "/s:" + userid,
+//						null)
+//				.toString();
+//		if (!"0".equals(tip)) {
+//			return model.resultMessage(4, "没有删除权限");
+//		}
 		return model.resultMessage(model.deleteMessage(mid), "删除留言成功");
 	}
 
